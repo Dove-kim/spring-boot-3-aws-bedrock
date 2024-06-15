93b3a05ed58d631a8b26ffeb59208dee20f49205
Mark <kimzasqwe@naver.com>
2024-06-15 21:33:02 +0900

    init project

---
+++ src/test/java/com/dpot/bedrock/BedrockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpot.bedrock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BedrockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
